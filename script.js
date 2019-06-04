a0df1492a560387a11693bdbff0d22f586ebaadf
ThomasAW2002 <42764375+ThomasAW2002@users.noreply.github.com>
2019-06-04 10:04:11 +0200

    html og css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Referanser til html-elementer
 const audio = document.querySelectorAll("audio");
-
+const divTrommer = document.querySelectorAll("tromme");
 // Globale variabler
 const knapper = ["a","s","d","f","g","h","j","k","l"];
 
@@ -9,8 +9,15 @@ document.onkeydown = function(event){
 
   for(let i = 0;i < 9; i++){
     if(event.key===knapper[i]){
-      audio[i].play()
+      divTrommer[i].classList.add("valgtTromme");
+      audio[i].play();
       audio[i].currentTime = 0;
     }
+  }
+}
+
+document.onkeyup = function(event){
+  for(let i = 0; i < 9; i++){
+    divTrommer[i].classList.remove("valgtTromme")
   }
 }
